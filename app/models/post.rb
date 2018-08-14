f127ea02dd3655143aee1f2e23af6df4d716c24a
github username <github email address>
2018-08-14 16:47:31 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :is_clickbait
+
+  def is_clickbait
+    if self.title.nil?
+      errors.add(:title, "Title is empty")
+      return
+    end
+
+    title_array = ["Won't Believe", "Secret", "Top [number]",  "Guess"]
+    title_array.each do |phrase|
+      if !self.title.scan(phrase).empty?
+        return true
+      end
+    end
+     errors.add(:title, "invalid title")
+  end
+
 end
