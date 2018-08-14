82484a110d2e2a8f80e7ef08939322c22d5982da
Kevin RRivera <krivera1015@gmail.com>
2018-08-14 15:31:04 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+  validate :must_be_clickbaity
+
+  def must_be_clickbaity
+      if !(self.title && self.title.match(/Won\'t Believe|Secret|Top \d|Guess/))
+        errors.add(:title, "Is not clickbaity")
+      end
+  end
 end
